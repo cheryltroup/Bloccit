6a5742bd3d42166adfeb0c3f9b262d6dee50a4c4
cheyltroup <cheryltroup@gmail.com>
2015-05-12 19:37:14 -0700

    Uploading Images Checkpoint Completed

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -17,11 +17,8 @@ class TopicsController < ApplicationController
   end
 
   def create
-    
-    #@topic = Topic.new(params.require(:topic).permit(:name, :description, :public))
     @topic = Topic.new(topic_params)
     authorize @topic
-
     if @topic.save
   	   redirect_to @topic, notice: "Topic was saved successfully."
     else
@@ -40,8 +37,6 @@ class TopicsController < ApplicationController
   def update
      @topic = Topic.find(params[:id])
      authorize @topic
-    
-     #if @topic.update_attributes(params.require(:topic).permit(:name, :desciption, :public)) 
      if @topic.update_attributes(topic_params)
         redirect_to @topic
      else
