34c812034885245727a4cde3962e346355e64681
cheyltroup <cheryltroup@gmail.com>
2015-06-04 14:04:57 -0700

    Public Profile Assignment Completed

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
+include Warden::Test::Helpers
+Warden.test_mode!
  
- describe "Visiting profiles" do
+describe "Visiting profiles" do
  
    include TestFactories
  
-   before do
+  before do
      @user = authenticated_user
      @post = associated_post(user: @user)
      @comment = Comment.new(user: @user, post: @post, body: "A Comment")
      allow(@comment).to receive(:send_favorite_emails)
      @comment.save!
-   end
+  end
  
-   describe "not signed in" do
+  describe "not signed in" do
  
      it "shows profile" do
        visit user_path(@user)
@@ -22,6 +24,21 @@ require 'rails_helper'
        expect( page ).to have_content(@post.title)
        expect( page ).to have_content(@comment.body)
      end
- 
-   end
- end
+  end 
+
+  describe "sign in" do
+
+    before do
+      login_as(@user, scope: :user)
+    end
+        
+    it "shows profile" do
+       visit user_path(@user)
+       expect(current_path).to eq(user_path(@user))
+
+       expect( page ).to have_content(@user.name)
+       expect( page ).to have_content(@post.title)
+       expect( page ).to have_content(@comment.body)
+    end
+  end
+end
