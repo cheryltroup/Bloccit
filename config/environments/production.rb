3c4b5d2a95f9b50ca1982c090c60f39410473db8
cheyltroup <cheryltroup@gmail.com>
2015-12-04 21:29:30 -0800

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
    # http://bloccit.herokuapp.com
-  config.action_mailer.default_url_options = { host: 'bloccit.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'cheryltroup-bloccit.herokuapp.com' }
 end
